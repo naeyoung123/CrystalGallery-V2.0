f704955f39fddb9cc52f54d94fb8c95f882ff0b6
naeyoung123 <naeyoung123@naver.com>
2022-01-15 17:04:50 +0900

    [Feat] 로그인, 회원가입 관련 페이지 생성 및 연결(account 폴더로 관리)

---
+++ routes/account/logout_process.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var template = require('../../lib/template.js');
+
+router.get('/logout_process', function(req,res){
+    if( req.session.is_logined){
+        req.session.destroy(function(err){
+            if(err) throw err;
+            res.redirect('/');
+        });
+    }
+    else{
+        res.redirect('/');
+    }
+});
+
+module.exports = router;
