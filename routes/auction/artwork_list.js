ff4db0da5015680fa943002fdb1010b785026501
naeyoung123 <naeyoung123@naver.com>
2022-01-15 17:06:19 +0900

    [feat] 경매 관련 페이지 생성 및 연결(auction 폴더로 관리)

---
+++ routes/auction/artwork_list.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var template = require('../../lib/template.js');
+var author = require('../../lib/author.js');
+const db = require('../../db.js');
+
+
+router.get('/artwork_list', function (request, response) {
+    var title = '작품 목록';
+    var authStatusUI = '<a class="nav-link" href="/login">로그인</a>';
+    var head = `
+    
+    `;
+    var body = `
+    
+    `;
+    var html = template.HTML(title, head, body, author.statusUI(request, response));
+    response.send(html);
+});
+
+module.exports = router;
