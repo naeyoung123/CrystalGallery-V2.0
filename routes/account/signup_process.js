f168f2805e24d8a49128b14f74f3ce511106a061
chaeeunkim <rlacodms3447@naver.com>
2022-01-23 16:39:01 +0900

    [Fix] signup_process.js 수정 : post로 받은 데이터 bodyparser로 처리하도록 수정

--- routes/account/signup_process.js
+++ routes/account/signup_process.js
@@ -4,28 +4,23 @@ var template = require('../../lib/template.js');
 const db = require('../../db.js');
 var qs = require('querystring');
 
-router.post('/signup_process', function (request, response) {
-    var body = ``;
-    request.on('data', function(data){
-        body = body + data;
-    });
-    request.on('end', function(){
-        var post = qs.parse(body);
-        var email = post.email;
-        var password = post.password;
-        var password_check = post.password_check;
-        var nickname = post.nickname;
-        if(password == password_check){
-            db.query(`INSERT INTO user (email, password, nickname) VALUES(?, ?, ?)`, [email, password, nickname], function(err, res){
-                if(err) {
-                    response.writeHead(200, {Location: `/signup`});
-                    response.end();
-                }
-                response.writeHead(302, {Location: `/`});
-                response.end();
-            });
+router.post('/signup_process', function(request, response) {
+
+    var post = request.body; //app.js에 만들어둔 bodyParser로 데이터 받기
+    var email = post.email;
+    var password = post.password;
+    var password_check = post.password_check;
+    var username = post.username;
+    if (password == password_check) {
+        db.query(`INSERT INTO user (email, username, password) VALUES(?, ?, ?)`, [email, username, password], function(err, res) {
+            if (err) {
+                response.writeHead(200, { Location: '/signup' });
+                v.end();
+            }
+            response.writeHead(302, { Location: '/' });
+            response.end();
+        });
     }
-    });
 });
 
 module.exports = router;
--- routes/main.js
+++ routes/main.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var template = require('../lib/template.js');
 var author = require('../lib/author.js');
 
-router.get('/', function (request, response) {
+router.get('/', function(request, response) {
     var title = 'Crystal Gallery';
     var authStatusUI = '<a class="nav-link" href="/login">로그인</a>';
     var head = ``;
