8d48dc776493212ba92fa4f992b3b2724c848b47
std0831 <std0831@naver.com>
2022-01-30 01:35:50 +0900

    [Feat] 작품 경매 페이지(artwork_auction.js) html, css 작성

--- routes/auction/artwork_auction.js
+++ routes/auction/artwork_auction.js
@@ -8,9 +8,70 @@ const db = require('../../db.js');
 router.get('/artwork_auction', function (request, response) {
     var title = '작품 경매';
     var head = `
-    
+        <style>
+        #wrapper {
+            width: 100%
+            height: 800px;
+            border: 1px solid #d1d1d1;
+            margin-top: auto;
+        }
+
+        #image {
+           width: 50%;
+           height: 500px;
+           float: left;
+           padding:10px;
+           border: 1px solid #d1d1d1;
+
+        }
+
+        #contents {
+            width: 50%;
+            height: 500px;
+            float: right;
+            text-align: left;
+            padding:10px;
+            
+        }
+
+        #comment {
+            claer: both;
+            height:100px;
+        }
+        </style>
     `;
+
     var body = `
+    <div id=wrapper>
+
+        <div id=image>
+            <img src=''>
+        </div>
+
+        <div id=contents>
+            <h3>작품명:</h3>
+            작품설명:
+            <br><br>
+            경매시작일: -경매마감일:
+            <br><br>
+            시작금액:
+            <br><br>
+            현재금액:
+            <br><br>
+            <label>
+            경매 금액: <input type="number" step="10" min="100" max="100000000"></label>
+            <br>
+            숫자 입력 : 10<input type="range" min="0" max="100000000">100000000
+
+            <br><br>
+            <input type="button" value="낙찰">
+        </div>
+
+        <div id=comment>
+        댓글을 입력하세요.
+        </div>
+
+    </div>
     
     `;
     var html = template.HTML(title, head, body, author.statusUI(request, response));
