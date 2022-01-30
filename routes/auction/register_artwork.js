cce421d33ce41b97aa05402159d39522881d23da
plum-king <jeesoo0908@gmail.com>
2022-01-30 13:47:27 +0900

    [Feat] register_artwork_process.js 추가 (작품 등록)

--- routes/auction/register_artwork.js
+++ routes/auction/register_artwork.js
@@ -1,12 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var template = require("../../lib/template.js");
 var author = require("../../lib/author.js");
 const db = require("../../db.js");
 
-router.get('/register_artwork', function (request, response) {
-    var title = '작품 등록';
-    var head = `
+router.get("/register_artwork", function (request, response) {
+  var title = "작품 등록";
+  var head = `
     <style>
         main>.container {
             padding: 60px 15px 0;
@@ -19,23 +19,24 @@ router.get('/register_artwork', function (request, response) {
         }
     </style>
     `;
-    var body = `
+  var body = `
     <main class="flex-shrink-0">
         <br><br><br>
         <div class="container">
         <center>
         <br><br>
             <h2>경매 작품을 등록하세요.</h2>
-            <form action="#" method="post">
-                작품명 <input type="text" placeholder="작품명">
+            <form action="/register_artwork_process" method="post" enctype="multipart/form-data">
+                작품명 <input type="text" name="art_name" placeholder="작품명">
                 <br>
-                시작 코인 </label><input type="number" placeholder="시작 코인">
+                작성자: ${request.session.username} <br>
+                시작 코인 </label><input type="number" name="initial_price" placeholder="시작 코인">
                 <br>
-                파일 첨부 <input type = "file">
+                파일 첨부 <input type = "file" name="art_file">
                 <br>
-                작품 설명 <textarea class="input" placeholder="작품 설명"></textarea>
+                작품 설명 <textarea class="input" name="art_explain" placeholder="작품 설명"></textarea>
                 <br>
-                경매 마감 시간 <input type = "datetime-local"><br>
+                경매 마감 시간 <input type = "datetime-local" name="time_ending"><br>
                 <div class="submit_btn">
                     <input id="submit_button" size="sm" type="submit" value="저장">
                 </div>
@@ -46,8 +47,13 @@ router.get('/register_artwork', function (request, response) {
         </div>
     </main>
     `;
-    var html = template.HTML(title, head, body, author.statusUI(request, response));
-    response.send(html);
+  var html = template.HTML(
+    title,
+    head,
+    body,
+    author.statusUI(request, response)
+  );
+  response.send(html);
 });
 
 module.exports = router;
