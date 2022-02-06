4782fc6b903f16b81a9700a4d022d8c0be2032cb
plum-king <jeesoo0908@gmail.com>
2022-02-07 04:05:36 +0900

    [Fix] DB 오류 해결 및 register_artwork_process 수정

--- routes/auction/artwork_auction.js
+++ routes/auction/artwork_auction.js
@@ -10,19 +10,22 @@ router.get("/artwork_auction/:listing_no", function (request, response) {
   /* url에서 listing_no 번호만 가져오기 */
   var listing_no = path.parse(request.params.listing_no).base;
   console.log("listing_no은" + listing_no + "입니다.");
-  const sql = "SELECT * FROM listing WHERE listing_no = ?";
+  const sql =
+    "SELECT art_bid_no, art_name, art_explain, initial_price, time_ending, time_starting, bid_participant, highest_bid, bid_upload_user FROM listing,bid WHERE listing.listing_no = bid.art_bid_no and listing_no = ?";
   db.query(sql, [listing_no], (err, row) => {
     if (err) {
       console.error(err);
     } else {
       var title = "작품 경매";
-      const list_no = row[0].listing_no;
+      const list_no = row[0].art_bid_no;
       const art_name = row[0].art_name;
       const initial_price = row[0].initial_price;
       const art_explain = row[0].art_explain;
       const time_ending = row[0].time_ending;
       const time_starting = row[0].time_starting;
-      console.log(list_no);
+      const highest_bid = row[0].highest_bid;
+      const bid_upload_user = row[0].bid_upload_user;
+      const user = request.session.username;
       var head = `
         <style>
         #wrapper {
@@ -72,10 +75,18 @@ router.get("/artwork_auction/:listing_no", function (request, response) {
             <br><br>
             시작금액: ${initial_price}
             <br><br>
-            현재금액: 
+            현재금액: ${highest_bid === null ? initial_price : highest_bid}
             <br><br>
+            ${
+              bid_upload_user === user
+                ? "자신의 작품에는 낙찰할 수 없습니다."
+                : "입찰 가능"
+            }
             <form action="/artwork_auction_process" method ="post">
             <input type="hidden" name = "list_no" value=${list_no}>
+            <input type="hidden" name = "highest_bid" value=${highest_bid}>
+            <input type="hidden" name = "bid_upload_user" value=${bid_upload_user}>
+            <input type="hidden" name = "initial_price" value=${initial_price}>
             <label>
             경매 금액: <input type="number" name ="bid_price" step="10" min="100" max="100000000"></label>
             <br>
