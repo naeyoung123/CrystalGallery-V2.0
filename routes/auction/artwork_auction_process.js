8d54d04f6b3a3df89835234a06dd30723f007e75
plum-king <jeesoo0908@gmail.com>
2022-02-12 15:43:06 +0900

    [Feat] auction_process 입찰 시 돈 차감 기능 및 프론트 요청사항 구현

--- routes/auction/artwork_auction_process.js
+++ routes/auction/artwork_auction_process.js
@@ -10,21 +10,22 @@ router.post("/artwork_auction_process", function (request, response) {
   var bid_price = post.bid_price;
   bid_price = parseInt(bid_price);
   var user = request.session.username;
-  // var bid_participant = post.bid_participant;
   var bid_upload_user = post.bid_upload_user;
   var highest_bid = post.highest_bid;
+  highest_bid = parseInt(highest_bid);
   var listing_no = post.list_no;
-  var initial_price = post.initial_price;
+  var bid_participant = post.bid_participant;
   console.log(user);
-  console.log(listing_no);
+  console.log(bid_participant);
   db.query(
+    // bid에서 해당하는 artwork 가져오기
     `SELECT * FROM bid WHERE art_bid_no=?`,
     [listing_no],
     function (err, res) {
       console.log(res[0]);
       if (res[0] === null) {
-        console.log("if내부");
         db.query(
+          // 만약 null이라면 bid에 값 넣기 -> null일 일도 없어서 02.19에 수정하겠음
           `INSERT INTO bid(bid_participant, art_bid_no, highest_bid) VALUES(?, ?, ?)`,
           [user, listing_no, bid_price],
           function (error, row) {
@@ -39,35 +40,132 @@ router.post("/artwork_auction_process", function (request, response) {
           }
         );
       } else {
-        console.log("else 내부");
+        // null이 아니라면
+        // 같은 사용자인지 확인하기 -> 프론트에서 처리돼서 여기까지 안들어와도 될듯
+        // 02.19에 수정하겠음
         if (user === bid_upload_user) {
-          //입찰자와 등록자가 동일하여 입찰 불가
-          //alert 띄우고 싶은데 02.13에 할 예정
           console.log("같은 사용자");
           response.writeHead(302, {
             Location: "/artwork_auction/" + listing_no,
           });
           response.end();
         } else {
+          // 입찰가랑 최고가랑 비교
           console.log(highest_bid, bid_price, listing_no);
           if (bid_price > highest_bid) {
-            console.log("비교");
-            //bid_price 가 highest_bid보다 작을 때는 아직 처리가 안돼서 이부분도 다음 주에 고치겠음
-            db.query(
-              `UPDATE bid SET bid_participant = ?, highest_bid = ? WHERE art_bid_no = ?`,
-              [user, bid_price, listing_no],
-              function (error, row) {
-                if (error) {
-                  console.error(error);
-                } else {
-                  response.writeHead(302, {
-                    Location: "/artwork_auction/" + listing_no,
-                  });
-                  response.end();
+            // 입찰가가 더 높으면
+            if (res[0].bid_participant === null) {
+              console.log("비교");
+              db.query(
+                `UPDATE bid SET bid_participant = ?, highest_bid = ? WHERE art_bid_no = ?`,
+                [user, bid_price, listing_no],
+                function (err2, row1) {
+                  if (err2) {
+                    console.error(err2);
+                  } else {
+                    // user 테이블을 검사하고 username이랑 동일한 bid_participant 즉, 지금 로그인한 입찰하려는 사람의 금액에서 차감
+                    db.query(
+                      `SELECT coin FROM user WHERE username = ?`,
+                      [user],
+                      function (err3, row2) {
+                        if (err3) {
+                          console.error(err3);
+                        } else {
+                          var coin = row2[0].coin;
+                          coin = parseInt(coin);
+                          coin -= bid_price;
+                          db.query(
+                            `UPDATE user SET coin = ? WHERE username = ?`,
+                            [coin, user],
+                            function (err4, row3) {
+                              if (err4) {
+                                console.error(err4);
+                              } else {
+                                response.writeHead(302, {
+                                  Location: "/artwork_auction/" + listing_no,
+                                });
+                                response.end();
+                              }
+                            }
+                          );
+                        }
+                      }
+                    );
+                  }
                 }
-              }
-            );
+              );
+            } else {
+              db.query(
+                //최고가로 입찰했던 사람 = bid_participant의 coin 찾기
+                `SELECT coin FROM user WHERE username =?`,
+                [bid_participant],
+                function (err2, row1) {
+                  if (err2) {
+                    console.error(err2);
+                  } else {
+                    console.log(bid_participant);
+                    var coin = row1[0].coin;
+                    coin = parseInt(coin);
+                    coin += highest_bid;
+                    //현재 입찰가보다 더 높은 금액으로 입찰한 사람이 생겨서 전 최고 입찰자에게 돈 돌려주기
+                    db.query(
+                      `UPDATE user SET coin = ? WHERE username = ?`,
+                      [coin, bid_participant],
+                      function (err3, row2) {
+                        if (err3) {
+                          console.error(err3);
+                        } else {
+                          db.query(
+                            // 현재 최고가로 입찰한 사람 = user
+                            `SELECT coin FROM user WHERE username =?`,
+                            [user],
+                            function (err4, row3) {
+                              if (err3) {
+                                console.error(err4);
+                              } else {
+                                var new_coin = row3[0].coin;
+                                new_coin = parseInt(new_coin);
+                                new_coin -= bid_price;
+                                db.query(
+                                  // 최고가로 입찰했기 때문에 coin을 bid_price만큼 깎아
+                                  `UPDATE user SET coin = ? WHERE username = ?`,
+                                  [new_coin, user],
+                                  function (err4, row3) {
+                                    if (err4) {
+                                      console.error(err4);
+                                    } else {
+                                      db.query(
+                                        // bid에서 최고가로 입찰한 사람 = participant와 현 최고가 = highest_bid 를 업데이트
+                                        `UPDATE bid SET bid_participant = ?, highest_bid = ? WHERE art_bid_no = ?`,
+                                        [user, bid_price, listing_no],
+                                        function (err5, row4) {
+                                          if (err5) {
+                                            console.error(err5);
+                                          } else {
+                                            response.writeHead(302, {
+                                              Location:
+                                                "/artwork_auction/" +
+                                                listing_no,
+                                            });
+                                            response.end();
+                                          }
+                                        }
+                                      );
+                                    }
+                                  }
+                                );
+                              }
+                            }
+                          );
+                        }
+                      }
+                    );
+                  }
+                }
+              );
+            }
           } else {
+            // 입찰가가 더 낮으면
             console.log("bid_price가 highest_bid보다 작거나 같은 경우");
             response.send(
               `<script type="text/javascript">alert("현재가보다 낮은 금액은 입찰하실 수 없습니다.");window.location.href = '/artwork_auction/' + ${listing_no} </script>;`
--- routes/profile/mypage.js
+++ routes/profile/mypage.js
@@ -1,21 +1,56 @@
-var express = require('express');
+var express = require("express");
+const session = require("express-session");
 var router = express.Router();
-var template = require('../../lib/template.js');
-var author = require('../../lib/author.js');
-const db = require('../../db.js');
+var template = require("../../lib/template.js");
+var author = require("../../lib/author.js");
+const db = require("../../db.js");
 
+//coin 확인 떄문에 body에 대충 틀만 넣었는데 이름이랑 이메일은 어차피 css 적용 전이라 안보이고
+//지금 들어가있는 틀 그냥 다 뜯어서 버려도 됩니다~!
+router.get("/mypage", function (request, response) {
+  var user = request.session.username;
+  const sql = `SELECT email, username, coin FROM user WHERE username = ?`;
+  db.query(sql, [user], (err, row) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const email = row[0].email;
+      const coin = row[0].coin;
 
-router.get('/mypage', function (request, response) {
-    var title = 'mypage';
-    var authStatusUI = '<a class="nav-link" href="/login">로그인</a>';
-    var head = `
+      var title = "mypage";
+      var authStatusUI = '<a class="nav-link" href="/login">로그인</a>';
+      var head = `
     
     `;
-    var body = `
-    
+      var body = `
+                        <h5>
+                            NAME:
+                            <strong style="font-size: 17px;color:lightslategrey;">
+                                ${user}
+                            </strong>
+                        </h5>
+                        <h5>
+                            E-mail: <strong
+                                style="font-size: 17px; color:lightslategrey;">${email}</strong>
+                        </h5>
+
+                        <div>
+                            <div>
+                                <div class="text-muted"><strong style="font-weight: bold;">COIN</strong></div>
+                                <div class="text-muted">${coin}</div>
+                            </div>
+                        </div>
+                    </div>
     `;
-    var html = template.HTML(title, head, body, author.statusUI(request, response));
-    response.send(html);
+      var html = template.HTML(
+        title,
+        head,
+        body,
+        author.statusUI(request, response)
+      );
+      response.send(html);
+    }
+  });
 });
 
 module.exports = router;
