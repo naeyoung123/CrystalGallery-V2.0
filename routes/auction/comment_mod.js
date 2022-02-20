d4be529f924d467fc87a5f876f919841b62e180f
plum-king <jeesoo0908@gmail.com>
2022-02-20 23:12:49 +0900

    [Design] comment mod, del 버튼 추가

---
+++ routes/auction/comment_mod.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+var template = require("../../lib/template.js");
+var author = require("../../lib/author.js");
+const db = require("../../db.js");
+var path = require("path");
+
+router.post("/comment_mod", function (request, response) {
+    
+});
+
+module.exports = router;
